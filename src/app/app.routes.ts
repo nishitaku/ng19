32ed32db40b470425a23cbc207a9cc98d51d4c5a
nishitaku <29690066+nishitaku@users.noreply.github.com>
2025-05-26 10:59:21 +0900

    feat: add mat-tab-nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { ChildAComponent } from './pages/child-a/child-a.component';
 import { ChildBComponent } from './pages/child-b/child-b.component';
+import { ChildCComponent } from './pages/child-c/child-c.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
       {
         path: 'child-b',
         component: ChildBComponent,
+      },
+      {
+        path: 'child-c',
+        component: ChildCComponent,
       },
       {
         path: '',
--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatTabsModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent {
+  links = ['child-a', 'child-b', 'child-c'];
+  activeLink = this.links[0];
+}
